91b989536695b7c72b36b83ec1892772043c961f
baboyiban <chl11wq12@gmail.com>
2023-01-02 12:26:40 +0900

    + src/main/resources/templates/html/test/test.html * src/main/java/com/example/demo/controller/healthController.java

--- src/main/java/com/example/demo/controller/healthController.java
+++ src/main/java/com/example/demo/controller/healthController.java
@@ -13,4 +13,8 @@ public class healthController {
 	public String main(Model m) {
 		return "html/freeboard";
 	}
+	@GetMapping("/test")
+	public String test(Model m) {
+		return "html/test/test";
+	}
 }
