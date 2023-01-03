cb6bb707ad08f7333c64889128485c93945c1b03
baboyiban <chl11wq12@gmail.com>
2023-01-03 17:30:01 +0900

    merge test <-> JongBin

---
+++ src/main/java/com/example/demo/controller/healthController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+@RequestMapping("/health")
+public class healthController {
+	@GetMapping("/freeboard")
+	public String main(Model m) {
+		return "html/freeboard";
+	}
+	
+	@GetMapping("/test")
+	public String test(Model m) {
+		return "html/test/test";
+	}
+}
