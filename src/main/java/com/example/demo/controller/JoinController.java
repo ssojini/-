37675a7c37db82cee64b151ef286fde46b5af506
clinamen <asdf3500@gmail.com>
2023-01-04 15:02:58 +0900

    add-2023-01-04

--- src/main/java/com/example/demo/controller/JoinController.java
+++ src/main/java/com/example/demo/controller/JoinController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.interfaces.JoinRepository;
+import com.example.demo.service.UserService;
 import com.example.demo.vo.User;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -23,6 +26,11 @@ public class JoinController
 {
 	@Autowired
 	private JoinRepository repo;
+	@Autowired
+	public HttpSession session;
+	@Autowired
+	public UserService svc;
+	
 	
 	@GetMapping("/join1")
 	public String showjoinForm1()
@@ -43,5 +51,62 @@ public class JoinController
 		return map;
 	}
 	
+	/*----------------- [상욱] ----------------- */
+	
+	@GetMapping("/add")
+	@ResponseBody
+	public String add()
+	{
+		Date date = Date.valueOf("2022-12-31");
+		User member = new User("smith","1234","clinamen",date,"010-1234-5678","smith@ezen.com","testID");
+		User added = repo.save(member);
+		return added.toString();
+	}
+	@GetMapping({"/","/login"})
+	public String login()
+	{
+		return "html/login/login";
+	}
+	@PostMapping("/login")
+	@ResponseBody
+	public Map<String,Object> loginProc(User user)
+	{					
+		return svc.login(user.getUserid(),user.getPwd());
+	}
+	@GetMapping("/findLoginInfo")
+	public String findLoginInfo()
+	{
+		return "html/login/findLoginInfo";
+	}
+	@GetMapping("/logon")
+	public String logon()
+	{
+		return "html/login/logon";
+	}
+	@PostMapping("/check")
+	@ResponseBody
+	public Map<String,Object> check(User user)
+	{
+		return svc.check(user.getUserid(),user.getEmail());
+	}
+	@PostMapping("/reset")
+	@ResponseBody
+	public Map<String,Object> reset(User user)
+	{
+		//System.err.println(user);
+		return svc.reset(user.getUserid(),user.getPwd());
+	}
+	@PostMapping("/find")
+	@ResponseBody
+	public Map<String,Object> find(User user)
+	{
+		return svc.find(user.getPhone(),user.getEmail());
+	}
+	@GetMapping("/logout")
+	public String logout()
+	{
+		session.setAttribute("userid", null);
+		return "html/login/login";
+	}
 	
 }
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,88 @@
+package com.example.demo.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.interfaces.UserRepository;
+import com.example.demo.vo.User;
+
+
+import jakarta.servlet.http.HttpSession;
+
+@Service
+public class UserService 
+{
+
+	@Autowired
+	public HttpSession session;
+	@Autowired
+	private UserRepository repo;
+	public Map<String, Object> login(String userid, String pwd) {
+		Map<String,Object> map = new HashMap<>();
+		User user = repo.findByUseridAndPwd(userid,pwd);
+		if(user!=null) {
+			map.put("user",user);
+			map.put("login",true);			
+			map.put("msg","로그인 성공");			
+			session.setAttribute("userid", user.getUserid());
+	
+		}else {
+			map.put("login",false);			
+			map.put("msg","로그인 실패");
+			session.setAttribute("userid", null);			
+		}
+		return map;
+	}
+	public Map<String, Object> check(String userid, String email) 
+	{
+		Map<String,Object> map = new HashMap<>();
+		User user = repo.findByUseridAndEmail(userid,email);
+		if(user!=null) {
+			map.put("userid", user.getUserid());
+			//System.err.println(user.getUserid());
+			map.put("msg", "checked");
+			map.put("checked", true);
+		}else {
+			map.put("checked", false);
+			map.put("userid", null);
+			map.put("msg", "No Account");
+		}
+		return map;
+	}
+	public Map<String, Object> reset(String userid, String pwd) 
+	{	
+		Map<String, Object> map = new HashMap<>();
+		Optional<User> user =repo.findById(userid);
+		user.get().setPwd(pwd);
+		if(repo.save(user.get())!=null)
+		{
+			map.put("msg", "비밀번호 변경 성공!");
+			map.put("reset", true);
+		}else {
+			map.put("msg", "변경 실패!");
+			map.put("reset", false);			
+		}
+		return map;
+	}
+	public Map<String, Object> find(String phone, String email) 
+	{
+		Map<String,Object> map = new HashMap<>();
+		User user = repo.findByPhoneAndEmail(phone,email);
+		if(user!=null) {
+			map.put("userid", user.getUserid());
+			map.put("found", true);
+		}else {
+			map.put("userid", null);
+			map.put("found", false);			
+		}
+		return map;
+	}
+
+		
+	
+	
+}
