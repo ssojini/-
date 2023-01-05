e44f7d44cb644fc6c948d809878259dc8a19ac84
sojinkim <sojinkim@218.38.137.28>
2023-01-05 10:38:34 +0900

    jhg

--- src/main/java/com/example/demo/controller/JoinController.java
+++ src/main/java/com/example/demo/controller/JoinController.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -65,28 +64,7 @@ public class JoinController
 		map.put("join", repo.save(user));
 		return map;
 	}
-	@GetMapping("/test")
-	@ResponseBody
-	public String sendTestMail()
-	{
-	    
-	    boolean isSent = es.sendHTMLMessage();
-	    
-	    return isSent ? "메일 보내기 성공":"메일 보내기 실패";
-	}
-	   
-	 @GetMapping("/auth/{code}")  // 보낸 메일에서 이용자가 인증 링크를 클릭했을 때
-	 @ResponseBody
-	 public String index(@PathVariable("code")String code,HttpSession session)
-	 {
-	 String scode = (String)session.getAttribute("cer");
-	 if(code.equals(scode))
-	 {
-		 return "인증완료 코드확인=" + code;
-	 }
-		 log.info("인증코드 확인={}", code);
-		 return "인증실패 인증코드확인="+code;
-	 }
+	
 	
 	/*----------------- [상욱] ----------------- */
 	
