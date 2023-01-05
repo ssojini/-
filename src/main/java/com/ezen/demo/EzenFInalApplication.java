b93d712f6f00bfec8c11164e0bb243079b5a8387
user <user@DESKTOP-0T8363E>
2023-01-05 12:53:09 +0900

    init

---
+++ src/main/java/com/ezen/demo/EzenFInalApplication.java
@@ -0,0 +1,13 @@
+package com.ezen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzenFInalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzenFInalApplication.class, args);
+	}
+
+}
