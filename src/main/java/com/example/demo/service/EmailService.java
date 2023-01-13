6729c5afa0e511525b3488a34fd094d2e991bf42
baboyiban <chl11wq12@gmail.com>
2023-01-13 12:14:59 +0900

    이메일 테스트

--- src/main/java/com/example/demo/service/EmailService.java
+++ src/main/java/com/example/demo/service/EmailService.java
@@ -27,135 +27,136 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class EmailService
 {
-   @Autowired
-   private JavaMailSender sender;
-   @Autowired
-   private HttpSession session;
-   
-   public boolean sendSimpleText()
-   {
-      List<String> receivers = new ArrayList<>();
-      receivers.add("siesta_w@naver.com");
+	@Autowired
+	private JavaMailSender sender;
+	@Autowired
+	private HttpSession session;
 
-      String[] arrReceiver = (String[])receivers.toArray(new String[receivers.size()]);
-      
-      SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-      
-      simpleMailMessage.setTo(arrReceiver); //수신자 설정
-      
-      simpleMailMessage.setSubject("Spring Boot Mail Test");
-      simpleMailMessage.setText("스프링에서 메일 보내기 테스트");
-      //SimpleMailMessage를 사용하여 html 을 전달하더라도 수신자의 화면에는 html이 해석되지 않음
-      simpleMailMessage.setText("<a href='/mail/auth/"+ createRandomStr()+"'>인증</a>");
-      
-      sender.send(simpleMailMessage);
+	public boolean sendSimpleText()
+	{
+		List<String> receivers = new ArrayList<>();
+		receivers.add("siesta_w@naver.com");
 
-      return true;
-   }
-   
-   
-   private String createRandomStr()
-   {
-	  //유일한 문자를 만드는 메소드
-      UUID randomUUID = UUID.randomUUID();
-      return randomUUID.toString().replaceAll("-", "");
-   }
-   
-   
-   public boolean sendMimeMessage()
-   {
-      MimeMessage mimeMessage = sender.createMimeMessage();
+		String[] arrReceiver = (String[])receivers.toArray(new String[receivers.size()]);
 
-      try {
-         InternetAddress[] addressTo = new InternetAddress[1];
-         addressTo[0] = new InternetAddress("siesta_w@naver.com");
+		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
 
-         mimeMessage.setRecipients(Message.RecipientType.TO, addressTo);
+		simpleMailMessage.setTo(arrReceiver); //수신자 설정
 
-         mimeMessage.setSubject("마임 메시지(Text) 테스트");
-         
-         mimeMessage.setContent("This is mimemessage", "text/plain;charset=utf-8");
-         
-         sender.send(mimeMessage);
-         return true;
-      } catch (MessagingException e) {
-         log.error("에러={}", e);
-      }
+		simpleMailMessage.setSubject("Spring Boot Mail Test");
+		simpleMailMessage.setText("스프링에서 메일 보내기 테스트");
+		//SimpleMailMessage를 사용하여 html 을 전달하더라도 수신자의 화면에는 html이 해석되지 않음
+		simpleMailMessage.setText("<a href='/mail/auth/"+ createRandomStr()+"'>인증</a>");
 
-      return false;
-   }
-   
-   public boolean checkmail(HttpSession session)
-   {
-	  String email = (String) session.getAttribute("email");
-	  System.err.println("email: "+email);
-	  String rdStr =createRandomStr();
-	  session.setAttribute("rdStr", rdStr);
-	  
-      MimeMessage mimeMessage = sender.createMimeMessage();
+		sender.send(simpleMailMessage);
 
-      try {
-         InternetAddress[] addressTo = new InternetAddress[1];
-         addressTo[0] = new InternetAddress(email);
+		return true;
+	}
 
-         mimeMessage.setRecipients(Message.RecipientType.TO, addressTo);
 
-         mimeMessage.setSubject("팀프로젝트 메일 확인");
-         
-         mimeMessage.setContent("<a href='http://localhost/team/auth/"+rdStr+"'>메일주소 인증</a>", "text/html;charset=utf-8");
-         
-         sender.send(mimeMessage);
-         return true;
-      } catch (MessagingException e) {
-         log.error("에러={}", e);
-      }
+	private String createRandomStr()
+	{
+		//유일한 문자를 만드는 메소드
+		UUID randomUUID = UUID.randomUUID();
+		return randomUUID.toString().replaceAll("-", "");
+	}
 
-      return false;
-   }
-   
-   public boolean sendAttachMail()
-   {
-      MimeMessage mimeMessage = sender.createMimeMessage();
-      
-      Multipart multipart = new MimeMultipart();
 
-      try {
-         InternetAddress[] addressTo = new InternetAddress[1];
-         addressTo[0] = new InternetAddress("siesta_w@naver.com");
+	public boolean sendMimeMessage()
+	{
+		MimeMessage mimeMessage = sender.createMimeMessage();
 
-         mimeMessage.setRecipients(Message.RecipientType.TO, addressTo);
+		try {
+			InternetAddress[] addressTo = new InternetAddress[1];
+			addressTo[0] = new InternetAddress("siesta_w@naver.com");
 
-         mimeMessage.setSubject("마임 메시지(첨부파일) 테스트");
-         
-         // Fill the message
-         BodyPart messageBodyPart = new MimeBodyPart();
-         String rdStr = createRandomStr();
-         session.setAttribute("rdStr",rdStr);
+			mimeMessage.setRecipients(Message.RecipientType.TO, addressTo);
 
-         messageBodyPart.setContent("<a href='http://localhost/mail/auth/"+rdStr+"'>메일주소 인증</a>", "text/html;charset=utf-8");
-         
-         multipart.addBodyPart(messageBodyPart);
-          
-         // Part two is attachment
-         messageBodyPart = new MimeBodyPart();
-         File file = new File("C:/test/첨부.txt");
-         FileDataSource fds = new FileDataSource(file);
-         messageBodyPart.setDataHandler(new DataHandler(fds));
-         
-         String fileName = fds.getName();
-         messageBodyPart.setFileName(fileName);
-         
-         multipart.addBodyPart(messageBodyPart);
-          
-         // Put parts in message
-         mimeMessage.setContent(multipart);
-         
-         sender.send(mimeMessage);
-         
-         return true;
-      }catch(Exception ex) {
-         log.error("에러={}", ex);
-      }
-      return false;
-   }
+			mimeMessage.setSubject("마임 메시지(Text) 테스트");
+
+			mimeMessage.setContent("This is mimemessage", "text/plain;charset=utf-8");
+
+			sender.send(mimeMessage);
+			return true;
+		} catch (MessagingException e) {
+			log.error("에러={}", e);
+		}
+
+		return false;
+	}
+
+	public boolean checkmail(HttpSession session)
+	{
+		String email = (String) session.getAttribute("email");
+		System.err.println("email: "+email);
+		String rdStr =createRandomStr();
+		session.setAttribute("rdStr", rdStr);
+
+		MimeMessage mimeMessage = sender.createMimeMessage();
+
+		try {
+			InternetAddress[] addressTo = new InternetAddress[1];
+			addressTo[0] = new InternetAddress(email);
+
+			mimeMessage.setRecipients(Message.RecipientType.TO, addressTo);
+
+			mimeMessage.setSubject("팀프로젝트 메일 확인");
+
+			//로컬호스트로 테스트시 
+			//mimeMessage.setContent("<a href='http://localhost/mail/auth/"+rdStr+"'>메일주소 인증</a>", "text/html;charset=utf-8");
+			//서버사용시 서버 IP주소 변경 할것
+			mimeMessage.setContent("<a href='http://192.168.0.111/mail/auth/"+rdStr+"'>메일주소 인증</a>", "text/html;charset=utf-8");
+
+			sender.send(mimeMessage);
+			return true;
+		} catch (MessagingException e) {
+			log.error("에러={}", e);
+		}
+
+		return false;
+	}
+
+	public boolean sendAttachMail()
+	{
+		MimeMessage mimeMessage = sender.createMimeMessage();
+
+		Multipart multipart = new MimeMultipart();
+
+		try {
+			InternetAddress[] addressTo = new InternetAddress[1];
+			addressTo[0] = new InternetAddress("siesta_w@naver.com");
+
+			mimeMessage.setRecipients(Message.RecipientType.TO, addressTo);
+
+			mimeMessage.setSubject("마임 메시지(첨부파일) 테스트");
+
+			// Fill the message
+			BodyPart messageBodyPart = new MimeBodyPart();
+			String rdStr = createRandomStr();
+			session.setAttribute("rdStr",rdStr);
+			messageBodyPart.setContent("<a href='http://localhost/mail/auth/"+rdStr+"'>메일주소 인증</a>", "text/html;charset=utf-8");
+			multipart.addBodyPart(messageBodyPart);
+
+			// Part two is attachment
+			messageBodyPart = new MimeBodyPart();
+			File file = new File("C:/test/첨부.txt");
+			FileDataSource fds = new FileDataSource(file);
+			messageBodyPart.setDataHandler(new DataHandler(fds));
+
+			String fileName = fds.getName();
+			messageBodyPart.setFileName(fileName);
+
+			multipart.addBodyPart(messageBodyPart);
+
+			// Put parts in message
+			mimeMessage.setContent(multipart);
+
+			sender.send(mimeMessage);
+
+			return true;
+		}catch(Exception ex) {
+			log.error("에러={}", ex);
+		}
+		return false;
+	}
 }
