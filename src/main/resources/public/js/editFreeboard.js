f70aa3eb4a597c7a68260ccd3ee48607106224dd
baboyiban <chl11wq12@gmail.com>
2023-01-18 14:21:35 +0900

    성공

--- src/main/resources/public/js/editFreeboard.js
+++ src/main/resources/public/js/editFreeboard.js
@@ -73,3 +73,22 @@ function updateContents() {
 		}
 	});
 }
+
+function deleteFreeboard(fbnum) {
+	$.ajax({
+		url:"/freeboard/delete",
+		method:"post",
+		data:{"fbnum":fbnum},
+		dataType:"json",
+		cache:false,
+		success:function(res) {
+			alert(res.result?"삭제 성공":"삭제 실패");
+			if (res.result) {
+				location.href = "/freeboard";
+			}
+		},
+		error:function(xhs,status,err) {
+			alert(err);
+		}
+	});
+}
