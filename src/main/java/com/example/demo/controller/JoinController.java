c079c019970f7814114515901627074dd81ddce7
pastebean <phchu0429@gmail.com>
2023-01-20 14:02:06 +0900

    2023.01.20

--- src/main/java/com/example/demo/controller/JoinController.java
+++ src/main/java/com/example/demo/controller/JoinController.java
@@ -45,7 +45,7 @@ public class JoinController
 	{
 		//상욱
 		Date date = Date.valueOf("2022-12-31");
-		User member = new User("asdf","1234","clinamen",date,"010-1234-5678","siesta_w@naver.com","testID");
+		User member = new User("asdf","1234","clinamen",date,"010-1234-5678","siesta_w@naver.com","/profile/default.png");
 		User added = repo.save(member);
 		
 		// 현주 
