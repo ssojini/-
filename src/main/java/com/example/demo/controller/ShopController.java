2b9bad2954a9ddac42c3c666aff381452004b5d5
clinamen <asdf3500@gmail.com>
2023-01-20 11:58:37 +0900

    구매하기 페이지 전달

--- src/main/java/com/example/demo/controller/ShopController.java
+++ src/main/java/com/example/demo/controller/ShopController.java
@@ -6,6 +6,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,7 +39,7 @@ public class ShopController {
 	private ShopService svc;
 	@Autowired
 	private GoodsRepository repo;
-	@Autowired //장바구니
+	@Autowired // 장바구니
 	private CartRepository cart_repo;
 
 	// 초기 테스트용
@@ -51,33 +55,46 @@ public class ShopController {
 	public String testAdd() {
 		// 상욱
 		// 이미지 경로 확인해야함
-		Goods goods = new Goods(0, "건강보조식품",  5000, "건강에 좋음", "상품에 관한 상세설명",  "카테고리1");
+		Goods goods = new Goods(0, "건강보조식품", 5000, "건강에 좋음", "상품에 관한 상세설명", "카테고리1");
 		Goods added = repo.save(goods);
-		goods = new Goods(0, "영양제",  1000, "건강에 좋음", "상품에 관한 상세설명",  "카테고리2");
+		goods = new Goods(0, "영양제", 1000, "건강에 좋음", "상품에 관한 상세설명", "카테고리2");
 		added = repo.save(goods);
-		Cart cart = new Cart(0,0,"테스트_영양제","ggods.png",100,1,100,"테스트","asdf");
+		Cart cart = new Cart(0, 0, "테스트_영양제", "goods.png", 100, 1, 100, "테스트", "asdf");
 		Cart c_added = cart_repo.save(cart);
-		//하드코딩 addgoods_att
+		// 하드코딩 addgoods_att
 
 		// 이후 부분에 추가하여 수정 요망
 
 		return "ShopController 초기 데이터 생성";
-	}	
+	}
 
 	/*--------------------- 상욱 시작 ----------------------*/
 
 	@GetMapping("/detail/{goodsnum}")
 	public String detail(@PathVariable("goodsnum") int goodsnum, Model m) {
 		// System.err.println(goodsnum);
+		// 상품정보
 		Goods goods = svc.getGoods(goodsnum);
 		m.addAttribute("goods", goods);
-		
+
+		// 상품사진
 		ArrayList<AddGoods_Att> attList = svc.getAddGoodsAtt(goodsnum);
 		m.addAttribute("attList", attList);
-		
+
+		// 상품 리뷰
+
+//		m.addAttribute("review", review);
+
 		return "html/shop/goodsDetail";
 	}
 
+	// 별점 리뷰페이지
+	@GetMapping("/detail/review")
+	public String review() {
+		return "html/shop/review";
+	}
+
+	// 장바구니 담기
 	@PostMapping("/cart")
 	@ResponseBody
 	public Map<String, Object> addCart(Cart cart) {
@@ -86,6 +103,7 @@ public class ShopController {
 		return map;
 	}
 
+	// 장바구니 보기
 	@GetMapping("/cart")
 	public String cart(Model m) {
 		// 로그인 연동 후 주석해제
@@ -93,42 +111,80 @@ public class ShopController {
 		// 연동 전 userid 하드코딩
 		String userid = "asdf";
 		ArrayList<Cart> cartlist = svc.getCart(userid);
-		//System.err.println("CART: "+cartlist);
+		// System.err.println("CART: "+cartlist);
 		// System.err.println(cartlist.get(0).getMainpic());
 		m.addAttribute("cartlist", cartlist);
 		return "html/shop/cart";
 	}
-	
-	//장바구니 수량변경
+
+	// 장바구니 수량변경
 	@PostMapping("/cnt_change")
 	@ResponseBody
-	public Map<String, Object> cnt_change(Cart cart)
-	{		
-		Map<String, Object> map = svc.cnt_change(cart.getCartnum(),cart.getProd_cnt());
+	public Map<String, Object> cnt_change(Cart cart) {
+		Map<String, Object> map = svc.cnt_change(cart.getCartnum(), cart.getProd_cnt());
 		return map;
 	}
-	
-	//장바구니 전체삭제
+
+	// 장바구니 전체삭제
 	@PostMapping("/delAll")
 	@ResponseBody
-	public Map<String, Object> delAll()
-	{		
+	public Map<String, Object> delAll() {
 		Map<String, Object> map = svc.delAll();
 		return map;
 	}
-	
-	//장바구니 선택삭제
+
+	// 장바구니 선택삭제
 	@PostMapping("/delSel")
 	@ResponseBody
-	public String ajaxTest(HttpServletRequest request) {
-		//System.err.println("controller start");
-		//String[] ajaxMsg = request.getParameterValues("valueArr");
-		//System.err.println("ajaxMsg1: "+ajaxMsg);
+	public String delSel(HttpServletRequest request) {
+		// System.err.println("controller start");
+		// String[] ajaxMsg = request.getParameterValues("valueArr");
+		// System.err.println("ajaxMsg1: "+ajaxMsg);
 		boolean delSel = svc.delSel(request);
-		//System.out.println("delSel: "+delSel);
+		// System.out.println("delSel: "+delSel);
 		return "redirect: cart";
 	}
-	
+
+	// 상품 구매
+	// 즉시 구매
+	@GetMapping("/buynow")
+	public String buyNow(Cart cart, Model m) {
+		// 구매목록을 orderList에 담아 보낸다.
+		m.addAttribute("orderlist", svc.buyNow(cart));
+		return "html/shop/orderItems";
+	}
+
+	// 장바구니 구매 (선택/전체)
+	@GetMapping("/buycart")
+	public String buyCart(@RequestParam String items, Model m) {
+
+		m.addAttribute("orderlist",svc.buyCart(items));
+
+		return "html/shop/orderItems";
+	}
+
+	@PostMapping("/buy")
+	@ResponseBody
+	public String buy(@RequestParam String paramList) {
+		System.err.println("here");
+
+		JSONParser parser = new JSONParser();
+		try {
+			JSONArray jsArr = (JSONArray) parser.parse(paramList);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+//		String json= parameters.get("paramList").toString();
+//		ObjectMapper mapper = new ObjectMapper();
+//	    List<Map<String, Object>> paramList = mapper.readValue(json, new TypeReference<ArrayList<Map<String, Object>>>(){});
+
+		// List<dto> paramList = mapper.readValue(json, new
+		// TypeReference<ArrayList<dto>>(){});
+		// System.err.println(itemArr);
+		return "성공";
+	}
 
 	/*--------------------- 상욱 끝 ----------------------*/
 
@@ -140,10 +196,11 @@ public class ShopController {
 		return "html/shop/AddGoods";
 
 	}
-	
-	@RequestMapping(value="/summer_image.do", produces = "application/json; charset=utf8")
+
+	@RequestMapping(value = "/summer_image.do", produces = "application/json; charset=utf8")
 	@ResponseBody
-	public String uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile, HttpServletRequest request ) throws IOException {
+	public String uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile,
+			HttpServletRequest request) throws IOException {
 		return svc.filesave(multipartFile);
 	}
 
