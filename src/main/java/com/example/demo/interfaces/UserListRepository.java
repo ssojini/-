5b59288212b864aeec41486c01f4c7adc9124807
kimminchae <kimminchae@localhost>
2023-02-02 12:46:12 +0900

    commit

--- src/main/java/com/example/demo/interfaces/UserListRepository.java
+++ src/main/java/com/example/demo/interfaces/UserListRepository.java
@@ -6,9 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.vo.UserJoin;
-import com.example.demo.vo.UserJoinJpa;
+import com.example.demo.vo.User;
 
-public interface UserListRepository extends JpaRepository<UserJoinJpa,String> {
-	Page<UserJoinJpa> findAll(Pageable pageable);
+public interface UserListRepository extends JpaRepository<User, String> {
+	Page<User> findAll(Pageable pageable);
 }
