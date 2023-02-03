f3ba4ee54da8bb0e34cada0bad66a9e1a82374f7
pastebean <phchu0429@gmail.com>
2023-02-03 14:52:19 +0900

    2023.02.03

--- src/main/java/com/example/demo/vo/AttachBoard.java
+++ src/main/java/com/example/demo/vo/AttachBoard.java
@@ -1,8 +1,5 @@
 package com.example.demo.vo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +7,8 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name="admin_board")
 public class AttachBoard 
 {
-	@Id
 	private int anum;
 	private int qnum;
 	private String attname;
