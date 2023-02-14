8790f1e6a94e94704b953d9bb9b187da383888cf
baboyiban <chl11wq12@gmail.com>
2023-02-14 14:56:28 +0900

    작업 중

---
+++ src/main/java/com/example/demo/security/HealthSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.demo.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class HealthSecurityConfig {
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		log.info("데이터소스 설정");
+		auth.jdbcAuthentication().dataSource(dataSource);
+	}
+	
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().requestMatchers("/resources/**","/ignore2");
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		log.info("접근 제한 설정");
+		return http.authorizeHttpRequests()
+				.anyRequest().authenticated() // 위의 설정 이외의 모든 요청은 인증을 거쳐야 한다
+				//.anyRequest().permitAll() // 위의 설정 이외의 모든 요청은 인증 요구하지 않음
+				
+				.and()
+				.formLogin().loginPage("/team/login")
+				.permitAll()
+				
+				.and()
+				.build();
+	}
+}
--- src/main/java/com/example/demo/vo/Main_Title.java
+++ src/main/java/com/example/demo/vo/Main_Title.java
@@ -1,11 +1,16 @@
 package com.example.demo.vo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name="main_title")
 public class Main_Title 
 {
+	@Id
 	private int num;
 	private String content;
 }
