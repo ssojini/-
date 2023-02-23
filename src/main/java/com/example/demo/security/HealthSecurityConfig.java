2a2ebc341fec56a5afb2682801800f0685d0098e
baboyiban <chl11wq12@gmail.com>
2023-02-23 16:36:46 +0900

    작업 중

--- src/main/java/com/example/demo/security/HealthSecurityConfig.java
+++ src/main/java/com/example/demo/security/HealthSecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.demo.security;
 
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -50,11 +52,72 @@ public class HealthSecurityConfig {
 	public WebSecurityCustomizer webSecurityCustomizer() {
 		return (web) -> web.ignoring().requestMatchers("/resources/**", "/ignore2");
 	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+				// 권한에 따른 인가
+				.authorizeHttpRequests()
+	            .requestMatchers( 
+	            		// get 방식 허용
+	            		"/css/**",
+	            		"/js/**",
+	            		"/images/**",
+	            		"/img/**",
+	            		"/health/main",
+	            		"/team/login",
+	            		"/team/findLoginInfo",
+	            		"/team/rules",
+	            		"/team/joinForm",
+	            		
+	            		// post 방식 허용 시에는 반드시 csrf에서도 똑같이 ignoring 해줘야 한다
+	            		"/team/sendemail",
+	            		"/team/authEmail",
+	            		"/team/join"
+	            		).permitAll()
+	            
+	            .anyRequest().authenticated() // 그 외 모든 요청은 인증된 사용자만 접근 가능
+	            
+	            // csrf
+	            .and()
+	            .csrf()
+	            .ignoringRequestMatchers(
+	            		// post 방식 허용 시에는 반드시 csrf에서도 똑같이 ignoring 해줘야 한다
+	            		"/team/sendemail",
+	            		"/team/authEmail",
+	            		"/team/join"
+	            		)
+	            
+	            // 로그인
+	            .and()
+				.formLogin().loginPage("/team/login") // 접속 차단시 로그인 페이지로 가게 하기
+				.loginProcessingUrl("/team/login") // post 로그인
+				.defaultSuccessUrl("/health/main", true) // 로그인 성공시 URL
+				.usernameParameter("userid")  // 로그인 폼에서 이용자 ID 필드 이름, 디폴트는 username
+				.passwordParameter("pwd")  // 로그인 폼에서 이용자 암호 필트 이름, 디폴트는 password
+				.permitAll()
+				
+				// 로그아웃
+				.and()   // 디폴트 로그아웃 URL = /logout
+				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/team/logout")) //로그아웃 요청시 URL
+				.logoutSuccessUrl("/team/login") // 로그아웃 성공시 URL
+				.invalidateHttpSession(true) 
+				.deleteCookies("JSESSIONID") // 세션 삭제
+				.permitAll()
+				
+				// 에러 페이지 구현하기
+				.and()
+				.exceptionHandling().accessDeniedPage("/team/denied")
+				
+				.and()
+				.build();
+	}
 
+	/*
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		log.info("접근제한 설정");
-		return http.authorizeHttpRequests()/* 권한에 따른 인가(Authorization) */
+		return http.authorizeHttpRequests()// 권한에 따른 인가(Authorization)
 				.requestMatchers("/css/*", "/js/*", "fragments/*", "/health/main", "/team/login", "/team/logout").permitAll()
 				.requestMatchers("/sec/hello").hasAnyRole("USER", "ADMIN")
 				.requestMatchers("/sec/getemps").hasAnyRole("USER", "ADMIN")
@@ -64,12 +127,12 @@ public class HealthSecurityConfig {
 				.anyRequest().authenticated()  // 위의 설정 이외의 모든 요청은 인증을 거쳐야 한다
 				//.anyRequest().permitAll()        // 위의 설정 이외의 모든 요청은 인증 요구하지 않음
 
-				//.and()
+				.and()
 				//.csrf().disable()    //csrf 기능을 사용하지 않을 때
 				//.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) //csrf 활성화
-				//.csrf()
-				//.ignoringRequestMatchers("/health/main")
-				//.ignoringRequestMatchers("/team/login")
+				.csrf()
+				.ignoringRequestMatchers("/css/*")
+				.ignoringRequestMatchers("/js/*")
 				//.ignoringAntMatchers("/csrf/score")
 				//.ignoringAntMatchers("/doLogin")
 				
@@ -95,9 +158,10 @@ public class HealthSecurityConfig {
 				.permitAll()
 
 				.and()
-				.exceptionHandling().accessDeniedPage("/sec/denied")
+				.exceptionHandling().accessDeniedPage("/team/denied")
 				.and().build();
 	}
+	*/
 
 	/*  아래의 내용은 In-Memory Authentication에 사용되므로 JDBC Authentication에는 불필요
 	@Autowired
