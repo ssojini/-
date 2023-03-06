faf62543aa4c86c4dfc87673ded533ca7239a487
baboyiban <chl11wq12@gmail.com>
2023-03-06 17:37:08 +0900

    Merge branch 'master' of https://github.com/pastebean/EzenFinal.git

--- src/main/java/com/example/demo/controller/JoinController.java
+++ src/main/java/com/example/demo/controller/JoinController.java
@@ -10,12 +10,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
