dd1ccc6fe9765b800ed3181dfac12e3b00fe3a93
baboyiban <chl11wq12@gmail.com>
2023-03-11 15:12:08 +0900

    commit

--- src/main/java/com/example/demo/controller/ShopController.java
+++ src/main/java/com/example/demo/controller/ShopController.java
@@ -215,7 +215,7 @@ public class ShopController {
 	@PostMapping("/addgoods")
 	@ResponseBody
 	public Map<String, Object> addgoods(@RequestParam("main_file") MultipartFile file,
-			@RequestParam("goods_detail") String goods_detail, @RequestParam("file[]") List<String> fileList,
+			@RequestParam("goods_detail") String goods_detail, @RequestParam(value="fileList[]") List<String> fileList,
 			HttpServletRequest request, Goods goods, AddGoods_Att att) {
 		boolean a =svc.storeFile(file, goods_detail, fileList, goods, att);
 		boolean added = false;
