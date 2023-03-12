6e281de702e6cea6bb98309379f773e178bd5117
baboyiban <chl11wq12@gmail.com>
2023-03-12 20:11:24 +0900

    * SecurityConfiguration.java - @EnableWebSecurity

--- src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +16,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import com.example.demo.service.UserService;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 	@Autowired
 	DataSource dataSource;    // JDBC Authentication에 필요함
